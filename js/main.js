bdfbce6c2758e386860122ee7ba263624a026727
fdsklnsdfsf <1035662410@qq.com>
2023-01-17 20:23:38 +0800

    Site updated: 2023-01-17 20:23:37

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let hideMenuIndex = ''
     if (window.innerWidth <= 768) hideMenuIndex = true
-    else hideMenuIndex = blogNameWidth + menusWidth + searchWidth > $nav.offsetWidth - 120
+    // else hideMenuIndex = blogNameWidth + menusWidth + searchWidth > $nav.offsetWidth - 120
 
     if (hideMenuIndex) {
       $nav.classList.add('hide-menu')
